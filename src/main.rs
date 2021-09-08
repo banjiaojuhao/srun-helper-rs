22bae41852552f8df5757f7942bf9ab6cc36b01d
banjiaojuhao <banjiaojuhao@qq.com>
2021-09-08 20:36:02 +0800

    get args from cmd args

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 use std::collections::HashMap;
-use std::env;
-use std::fs::File;
-use std::io::Read;
 use std::net::IpAddr;
 use std::str::FromStr;
 
+use argh::FromArgs;
 use chrono::Utc;
 use hex;
 use md5::Digest;
@@ -16,59 +14,65 @@ use crate::encrypt::x_encode_str;
 
 mod encrypt;
 
-struct User {
-    username: String,
-    password: String,
-}
 
 const HOST_URL: &str = "http://10.248.98.2/cgi-bin/srun_portal";
 const CHALLENGE_URL: &str = "http://10.248.98.2/cgi-bin/get_challenge";
 
+#[derive(FromArgs, PartialEq, Debug)]
+/// login or logout for srun
+struct LoginInfo {
+    #[argh(subcommand)]
+    action: Action,
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+#[argh(subcommand)]
+enum Action {
+    Login(Login),
+    Logout(Logout),
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+#[argh(subcommand, name = "login")]
+/// login srun
+struct Login {
+    #[argh(option, short = 'u')]
+    /// username
+    username: String,
+
+    #[argh(option, short = 'p')]
+    /// password
+    password: String,
+
+    #[argh(option, short = 'I')]
+    /// interface address
+    interface: Option<String>,
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+#[argh(subcommand, name = "logout")]
+/// logout srun
+struct Logout {
+    #[argh(option, short = 'u')]
+    /// username
+    username: String,
+
+    #[argh(option, short = 'I')]
+    /// interface address
+    interface: Option<String>,
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("usage: srun-helper-rs login [local address]");
-        return;
-    };
-    let action = args[1].as_str();
+    let log_info: LoginInfo = argh::from_env();
 
-    let mut user = String::new();
-    File::open("user.txt")
-        .expect("failed to open user.txt(username;password)")
-        .read_to_string(&mut user)
-        .expect("failed to read user.txt");
-
-    let user: Vec<&str> = user.trim().splitn(2, ";").collect();
-    if user.len() != 2 {
-        println!("invalid user.txt");
-        return;
-    };
-    let user = User {
-        username: user[0].trim().to_owned(),
-        password: user[1].trim().to_owned(),
-    };
-
-    let web_client = if args.len() > 2 {
-        let local_address = args[2].as_str();
-        reqwest::blocking::Client::builder()
-            .local_address(IpAddr::from_str(local_address).unwrap())
-            .build()
-            .unwrap()
-    } else {
-        reqwest::blocking::Client::new()
-    };
-
-    let result = match action {
-        "login" => {
-            login(&web_client, &user)
+    let result = match log_info.action {
+        Action::Login(login_config) => {
+            let web_client = get_web_client(&login_config.interface);
+            login(&web_client, &login_config.username, &login_config.password)
         }
-        "logout" => {
-            logout(&web_client, &user)
-        }
-        _ => {
-            println!("invalid args. (only \"login\" or \"logout\")");
-            return;
+        Action::Logout(logout_config) => {
+            let web_client = get_web_client(&logout_config.interface);
+            logout(&web_client, &logout_config.username)
         }
     };
 
@@ -77,23 +81,23 @@ fn main() {
     println!("({},{})", res_str, error_msg_str);
 }
 
-fn login(web_client: &Client, user: &User) -> Value {
+fn login(web_client: &Client, username: &str, password: &str) -> Value {
     let callback_str = get_callback();
-    let challenge = get_challenge(&web_client, &user, callback_str.as_str());
+    let challenge = get_challenge(&web_client, username, callback_str.as_str());
 
     let client_ip = challenge["client_ip"].as_str().unwrap();
     let token = challenge["challenge"].as_str().unwrap();
 
     let mut user_info: HashMap<&str, &str> = HashMap::new();
-    user_info.insert("username", user.username.as_str());
-    user_info.insert("password", user.password.as_str());
+    user_info.insert("username", username);
+    user_info.insert("password", password);
     user_info.insert("ip", client_ip);
     user_info.insert("ac_id", "1");
     user_info.insert("enc_ver", "srun_bx1");
     let data_json = serde_json::to_string(&user_info).unwrap();
 
     let mut login_form = HashMap::new();
-    login_form.insert("username", user.username.as_str());
+    login_form.insert("username", username);
     login_form.insert("action", "login");
     login_form.insert("n", "200");
     login_form.insert("type", "1");
@@ -107,14 +111,14 @@ fn login(web_client: &Client, user: &User) -> Value {
 
     let mut digest = md5::Md5::new();
     digest.update(token.as_bytes());
-    digest.update(user.password.as_bytes());
+    digest.update(password.as_bytes());
     let hmd5: String = hex::encode(digest.finalize().iter());
     let md5_str = "{MD5}".to_owned() + &hmd5;
     login_form.insert("password", &md5_str);
 
     let mut checksum = Sha1::new();
     checksum.update(token.as_bytes());
-    checksum.update(user.username.as_bytes());
+    checksum.update(username.as_bytes());
     checksum.update(token.as_bytes());
     checksum.update(hmd5.as_bytes());
     checksum.update(token.as_bytes());
@@ -141,12 +145,11 @@ fn login(web_client: &Client, user: &User) -> Value {
     return result;
 }
 
-
-fn logout(web_client: &Client, user: &User) -> Value {
+fn logout(web_client: &Client, username: &str) -> Value {
     let callback: String = get_callback();
     let mut data: HashMap<&str, &str> = HashMap::new();
     data.insert("action", "logout");
-    data.insert("username", user.username.as_str());
+    data.insert("username", username);
     data.insert("ac_id", "1");
     data.insert("ip", "");
     data.insert("callback", callback.as_str());
@@ -162,13 +165,27 @@ fn logout(web_client: &Client, user: &User) -> Value {
     return result;
 }
 
+fn get_web_client(interface_config: &Option<String>) -> Client {
+    match interface_config {
+        None => {
+            reqwest::blocking::Client::new()
+        }
+        Some(local_address) => {
+            reqwest::blocking::Client::builder()
+                .local_address(IpAddr::from_str(&local_address).unwrap())
+                .build()
+                .unwrap()
+        }
+    }
+}
+
 fn get_callback() -> String {
     return format!("jsonp{}", Utc::now().timestamp_millis());
 }
 
-fn get_challenge(web_client: &Client, user: &User, callback: &str) -> Value {
+fn get_challenge(web_client: &Client, username: &str, callback: &str) -> Value {
     let mut data: HashMap<&str, &str> = HashMap::new();
-    data.insert("username", user.username.as_str());
+    data.insert("username", username);
     data.insert("callback", callback);
     let resp = web_client.get(CHALLENGE_URL).query(&data).send()
         .expect("failed to send request for challenge");
